edbee9c927c654970a6bd3f81f28e9252d3fa80f
MaciejBodnar <maciej.bodnar2@gmail.com>
2024-03-13 14:14:54 +0100

    styling and rendering sum

--- index.js
+++ index.js
@@ -10,16 +10,24 @@ let isAlive = true;
 
 let message = ''; //replacing all consolelog with empty variable and console login once
 
+let messageEl = document.getElementById('message-el');
+let messageHide = document.getElementById('message-hide');
+// let sumEl = document.getElementById('sum-el');
+let sumEl = document.querySelector('#sum-el');
+
 function startGame() {
+  messageHide.textContent = '';
+  sumEl.textContent += sum + ' ';
+
   if (sum <= 20) {
     message = 'Do you want to draw a new card?';
   } else if (sum === 21) {
-    message = 'You won!';
+    message = 'You won! BlackJack!';
     hasBlackJack = true; // change boolean hasBlackJack
   } else {
     message = "You're out of the game!";
     isAlive = false;
   }
   // Log out
-  console.log(message);
+  messageEl.textContent = message;
 }
